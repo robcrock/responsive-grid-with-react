5907c9ce18c13276d366d5f671b49157408f93cc
robcrockPI <robert@propellerindustries.com>
2020-11-24 17:35:12 -0300

    Remove the app.css file, folder and import.

--- src/components/styles/GridStyle.js
+++ src/components/styles/GridStyle.js
@@ -2,9 +2,9 @@ import styled from "styled-components"
 
 const Grid = styled.div`
   display: grid;
-  max-width: 1200px;
+  max-width: 1100px;
   margin: auto;
-  grid-gap: 1rem;
+  grid-gap: 1.5rem;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 `
 export default Grid
