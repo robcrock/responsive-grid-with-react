d274a94cb32bc296fd76e555499db07308682506
robcrockPI <robert@propellerindustries.com>
2020-11-25 18:55:06 -0300

    Clean up extraneous code and remove the utild directory.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,20 @@
-import { useRef, useState, useEffect } from "react"
+import { useRef, useEffect } from "react"
 import CardStyle from "../styles/CardStyle"
 
 const { tableau } = window
 
 export default function Card({ dashboard }) {
   const ref = useRef(null)
-  const [viz, setViz] = useState(null)
 
   useEffect(() => {
-    console.dir("Loading " + dashboard)
+    const viz = new tableau.Viz(ref.current, `${dashboard}`, {
+      hideTabs: true,
+      hideToolbar: true,
+      width: "290px",
+      height: "180px",
+    })
 
-    setViz(
-      new tableau.Viz(ref.current, `${dashboard}`, {
-        hideTabs: true,
-        hideToolbar: true,
-        width: "290px",
-        height: "180px",
-      })
-    )
-
-    return () => viz.dispose()
+    return viz;
   }, [])
 
   return (
--- src/styles/CardStyle.js
+++ src/styles/CardStyle.js
@@ -5,10 +5,6 @@ const Card = styled.div`
   padding: 16px;
   border-radius: 5px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  :hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-  }
 `
 
 export default Card
