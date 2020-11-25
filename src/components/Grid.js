c4b5c863ee066a8a0fcde406afc0c5a56f3e3aef
robcrockPI <robert@propellerindustries.com>
2020-11-25 19:05:30 -0300

    Move dashboards within the Grid component.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,22 +2,23 @@ import GridStyle from "../styles/GridStyle"
 import { v4 as uuidv4 } from "uuid"
 import Card from "./Card"
 
-const dashboards = [
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi1",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi2",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi3",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi4",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi5",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi6",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi7",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi8",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi9",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi10",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi11",
-  "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi12",
-]
-
 export default function Grid() {
+
+  const dashboards = [
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi1",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi2",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi3",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi4",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi5",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi6",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi7",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi8",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi9",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi10",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi11",
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/SocialMediaKPIs/kpi12",
+  ]
+
   return (
     <GridStyle>
       {dashboards.map((dashboard) => (
